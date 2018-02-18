119e87b8ab28d9a1dfa8cdf96a0ac2751a638771
Climente Alin <aclimente@ymail.com>
2018-02-18 17:04:27 +0200

    Add files via upload

--- df2db.py
+++ df2db.py
@@ -23,21 +23,26 @@ Requires pandas module to be installed.
 """
 
 
-# In[2]:
+# In[68]:
 
 
 class Df2db:
     
     def __init__(self, dbname, root_path):
         self.dbname = dbname
-        self.root_path = root_path
+        self.root_path = root_path    
     
     def connect_db(self):
         #Connect to a db and if it not exists creates one with the name given
         connection = sqlite3.connect(self.dbname)
         cursor = connection.cursor()
         return connection, cursor
-        
+    
+    def close(self):
+        connection, cursor = Df2db(self.dbname, self.root_path).connect_db()
+        connection.commit()
+        connection.close()
+    
     def norm_punctmarcks(self, astring):
         #Removing punctuation marks from the string, necessary for making compatible table names
         punctuation_marks = list(str(string.punctuation).replace('_', ''))+[' ']
@@ -48,42 +53,82 @@ class Df2db:
             pass
         return astring
     
-    def df_tosql(self, path_to_xlname):
+    def rename_duplicate_dfcols(self, df):
+        #Rename DF columns if found duplicates (credit to SO"Lamakaha")
+        try:
+            cols=pd.Series(df.columns)
+            for dup in df.columns.get_duplicates(): 
+                cols[df.columns.get_loc(dup)]=[dup+'.'+str(d_idx) if d_idx!=0 else dup for d_idx in range(df.columns.get_loc(dup).sum())]
+                df.columns=cols
+        except Exception as e:
+            print("Got ", e)
+            pass
+        return df
+    
+    
+    def df_tosql(self, path_to_xlname, dfname=''):
         #Get thru all sheets and if it has data save it to db
         connection, cursor = Df2db(self.dbname, self.root_path).connect_db()
         extension = path_to_xlname[-4:]
-        if re.search('xlsx', extension) or re.search('xls', extension) or re.search('xlsm', extension):
-            #If excel file then
-            xl = pd.ExcelFile(path_to_xlname)
-            for sht in xl.sheet_names:
-                df_sht = xl.parse(sht)
-            if df_sht.shape == (0,0): 
-                pass
+        try:
+            if re.search('xlsx', extension) or re.search('xls', extension) or re.search('xlsm', extension):
+                #If excel file then
+                xl = pd.ExcelFile(path_to_xlname)
+                for sht in xl.sheet_names:
+                    df_sht = xl.parse(sht)
+                if df_sht.shape == (0,0): 
+                    pass
+                else:
+                    xlname = path_to_xlname.strip().split('\\')[-1]
+                    tablename_insql = str('EXCEL_' + xlname + '_SHEET_' + sht)
+                    tablename_insql = Df2db(self.dbname, self.root_path).norm_punctmarcks(tablename_insql)
+                    #Replacing spaces with underscores to be compatible to sql tables
+                    df_sht.rename(columns=lambda x: x.strip().replace(' ', '_'), inplace=True)
+                    #Rename duplicate columns from df
+                    df_sht = Df2db(self.dbname, self.root_path).rename_duplicate_dfcols(df_sht)
+                    #Saving df to sqlite3 db
+                    df_sht.to_sql(tablename_insql, connection, if_exists="replace", index=False)
+                    connection.commit()
+                    print('{} ok'.format(tablename_insql))
+            elif re.search('.csv', extension):
+                #If csv file then
+                xl = pd.read_csv(path_to_xlname)   
+                if xl.shape == (0,0): 
+                    pass
+                else:
+                    xlname = path_to_xlname.strip().split('\\')[-1]
+                    tablename_insql = str('EXCEL_' + xlname + '_SHEET_Sheet1')
+                    tablename_insql = Df2db(self.dbname, self.root_path).norm_punctmarcks(tablename_insql)
+                    xl.rename(columns=lambda x: x.strip().replace(' ', '_'), inplace=True)
+                    #Rename duplicate columns from df
+                    xl = Df2db(self.dbname, self.root_path).rename_duplicate_dfcols(xl)
+                    xl.to_sql(tablename_insql, connection, if_exists="replace", index=False)
+                    connection.commit()
+                    print('{} ok'.format(tablename_insql))
             else:
-                xlname = path_to_xlname.strip().split('\\')[-1]
-                tablename_insql = str('EXCEL_' + xlname + '_SHEET_' + sht)
+                print('{} NOT SAVED!'.format(path_to_xlname))
+                pass
+        except:
+            try:
+                if dfname == '': 
+                    print("Insert the df and the dfname!!")
+                    sys.exit()
+                df = path_to_xlname
+                tablename_insql = dfname
+                tablename_insql = str('EXCEL_' + tablename_insql + '_xlsx_SHEET_Sheet1')
                 tablename_insql = Df2db(self.dbname, self.root_path).norm_punctmarcks(tablename_insql)
-                #Replacing spaces with underscores to be compatible to sql tables
-                df_sht.rename(columns=lambda x: x.strip().replace(' ', '_'), inplace=True)
-                #Saving df to sqlite3 db
-                df_sht.to_sql(tablename_insql, connection, if_exists="replace", index=False)
+                df.rename(columns=lambda x: x.strip().replace(' ', '_'), inplace=True)
+                #Rename duplicate columns from df
+                df = Df2db(self.dbname, self.root_path).rename_duplicate_dfcols(df)
+                df.to_sql(tablename_insql, connection, if_exists="replace", index=False)
                 connection.commit()
                 print('{} ok'.format(tablename_insql))
-        elif re.search('.csv', extension):
-            #If csv file then
-            xl = pd.read_csv(path_to_xlname)   
-            if xl.shape == (0,0): 
+            except Exception as e:
+                print('NOT SAVED! \n{}'.format(e))
                 pass
-            else:
-                xlname = path_to_xlname.strip().split('\\')[-1]
-                tablename_insql = str('EXCEL_' + xlname + '_SHEET_' + 'Sheet1')
-                tablename_insql = Df2db(self.dbname, self.root_path).norm_punctmarcks(tablename_insql)
-                xl.rename(columns=lambda x: x.strip().replace(' ', '_'), inplace=True)
-                xl.to_sql(tablename_insql, connection, if_exists="replace", index=False)
-                connection.commit()
-                print('{} ok'.format(tablename_insql))
-        else:
-            print('{} NOT SAVED!'.format(path_to_xlname))
+            
+            
+            
             
     def getdf_fromdb(self, table_name):
         #gets the table from the db
@@ -151,6 +196,74 @@ class Df2db:
             for df in path_to_dfs:
                 Df2db(self.dbname, self.root_path).df_tosql(df)
         except Exception as e:
-            print("nok", e)
+            print("Error: {},\n{}".format(e, df))
             pass
+        
+    
+
+
+# In[ ]:
+
+
+"""
+#Import class Df2db from df2db module
+from df2db import Df2db
+
+#Instantiate with the database name and the path where you got excel files
+todf = Df2db('dbname.db', r'D:\alot_of_xlfiles')
+
+#Call dfs_tosql function to search in the path you give for excel files 
+#and save them to the database with the name given
+todf.dfs_tosql()
+
+#Show tables from the database
+todf.show_db_tables()
+
+#Get a table from the database as a dataframe object
+#in order to use it in pandas for manipulation
+todf.getdf_fromdb("EXCEL_xl4_xlsx_SHEET_Sheet1")
+
+#Delete a table from the database
+todf.drop_tablefrom_db("EXCEL_xl4_xlsx_SHEET_Sheet1")
+
+#Show tables from db, now you see one it's gone
+todf.show_db_tables()
+
+#Close the connection when you are done.
+todf.close()
+
+#Instantiate with the database name and the path where you got excel files
+todf = Df2db('dbname.db', r'D:\alot_of_xlfiles')
+
+#Make a dataframe
+import numpy as np
+import pandas as pd
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
+
+#Save it to sql, you must give the df and a name for the df
+todf.df_tosql(df, 'test')
+
+#Show current tables from db 
+todf.show_db_tables()
+#Close when you are done
+todf.close()
+
+#Each table saved in database will have this form
+tablname = "EXCEL_test_xlsx_SHEET_Sheet1"
+tablname.split('_')
+
+
+'EXCEL' - all tables in db will start with this prefix
+'test' - the name if the excel or df 
+'xlsx' - the extension of the file
+'SHEET' - the next folowing this will be the Sheet name of the excel
+That's because it looks in the workbook in all sheets for tables and saves the sheet name also
+'Sheet1' - The sheet where the table was found
+
+If the excel or df has punctuation marks or spaces those will be replaced with underscore "_"
+This replace is done in order to be compatible with sqlite3 database
+Also, if the column names contains spaces those will be replaced with "_"
+
+"""
 
